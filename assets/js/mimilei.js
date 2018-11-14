d23c97d81b89e83d3eb0a46cc29d9610864bf7fc
Michelle Chang <cowabunga11115@gmail.com>
2018-11-14 02:59:35 -0800

    timeout

--- assets/js/mimilei.js
+++ assets/js/mimilei.js
@@ -1,6 +1,16 @@
 $(".subtitle.work").on("click", function(e){ 
     console.log("Clicked a creative category");
-    $(".scroll").load("creative/" + this.id + ".html", function() {
+    $(".scroll").load("creative/" + this.id + ".html"); 
+    console.log("after loading call");
+    e.preventDefault(); // cancel the click
+    var allSubtitles = $(".subtitle.work");
+    var i;
+    for (i = 0; i < allSubtitles.length; i++) {
+        allSubtitles[i].style.fontWeight = 300;
+    }
+    console.log(this.id);
+    this.style.fontWeight = 600;
+    window.setTimeout(function() {
         $(".scroll_window").mCustomScrollbar("destroy");
         $(".scroll_window").mCustomScrollbar({
             axis:"x", // horizontal scrollbar
@@ -14,16 +24,7 @@ $(".subtitle.work").on("click", function(e){
                 }
             },
         });
-    }); 
-    console.log("after loading call");
-    e.preventDefault(); // cancel the click
-    var allSubtitles = $(".subtitle.work");
-    var i;
-    for (i = 0; i < allSubtitles.length; i++) {
-        allSubtitles[i].style.fontWeight = 300;
-    }
-    console.log(this.id);
-    this.style.fontWeight = 600;
+    }, 2000);
   });
 
 $(window).on("wheel", function(e){

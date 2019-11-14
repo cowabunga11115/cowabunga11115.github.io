3733664d4cfc1c8132677470d65a4834ddaca493
Michelle Chang <mimilei@mimilei-macbookpro2.roam.corp.google.com>
2019-11-14 14:33:17 -0800

    Adding mini album art for mobile view

---
+++ assets/js/horizontal_gallery.js
@@ -0,0 +1,37 @@
+$(".subtitle.work").on("click", function(e){
+    console.log("Clicked a creative category");
+    $(".scroll").load("creative/" + this.id + ".html");
+    console.log("after loading call");
+    e.preventDefault(); // cancel the click
+    var allSubtitles = $(".subtitle.work");
+    var i;
+    for (i = 0; i < allSubtitles.length; i++) {
+        allSubtitles[i].style.fontWeight = 300;
+    }
+    console.log(this.id);
+    this.style.fontWeight = 600;
+    window.setTimeout(function() {
+        $(".scroll_window").mCustomScrollbar("destroy");
+        $(".scroll_window").mCustomScrollbar({
+            axis:"x", // horizontal scrollbar
+            scrollInertia: 5,
+            advanced:{ updateOnContentResize: true },
+            advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
+            advanced:{ updateOnImageLoad: true },
+            callbacks:{
+                onUpdate:function(){
+                    console.log("Scrollbars updated");
+                }
+            },
+        });
+    }, 2000);
+  });
+
+$(window).on("wheel", function(e){
+    var $div = $('.content');
+    // set div scroll top offset to current + extra from this scroll
+    $div.scrollTop($div.scrollTop()
+                    - e.originalEvent.wheelDelta);
+
+    return false; // prevent body scrolling
+});
--- assets/js/mimilei.js
+++ assets/js/mimilei.js
@@ -3,56 +3,54 @@ const album_storment = "Depression visualization";
 const album_lumos = "Hospital ambiance controller";
 const album_cocoon = "Noise pollution awareness for expectant mothers";
 
+// Keeps track of the current icon type associated with title
+// If null, the user is on mobile, or there is no icon associated.
 var curr_icon;
+
 // MAIN ======================================================================
-$( document ).ready(function() {
-  console.log( "ready!" );
-  adjustTitleStyle();
-  maybeShowPjAlbumArt();
-});
+adjustTitleStyle();
+maybeShowPjAlbumArt();
 window.addEventListener('resize', adjustTitleStyle);
 window.addEventListener('resize', maybeShowPjAlbumArt);
 
-document.addEventListener('touchstart', function(){}, {passive: false})
-
 // JQUERY ======================================================================
-$(".subtitle.work").on("click", function(e){
-    console.log("Clicked a creative category");
-    $(".scroll").load("creative/" + this.id + ".html");
-    console.log("after loading call");
-    e.preventDefault(); // cancel the click
-    var allSubtitles = $(".subtitle.work");
-    var i;
-    for (i = 0; i < allSubtitles.length; i++) {
-        allSubtitles[i].style.fontWeight = 300;
-    }
-    console.log(this.id);
-    this.style.fontWeight = 600;
-    window.setTimeout(function() {
-        $(".scroll_window").mCustomScrollbar("destroy");
-        $(".scroll_window").mCustomScrollbar({
-            axis:"x", // horizontal scrollbar
-            scrollInertia: 5,
-            advanced:{ updateOnContentResize: true },
-            advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
-            advanced:{ updateOnImageLoad: true },
-            callbacks:{
-                onUpdate:function(){
-                    console.log("Scrollbars updated");
-                }
-            },
-        });
-    }, 2000);
-  });
+// $(".subtitle.work").on("click", function(e){
+//     console.log("Clicked a creative category");
+//     $(".scroll").load("creative/" + this.id + ".html");
+//     console.log("after loading call");
+//     e.preventDefault(); // cancel the click
+//     var allSubtitles = $(".subtitle.work");
+//     var i;
+//     for (i = 0; i < allSubtitles.length; i++) {
+//         allSubtitles[i].style.fontWeight = 300;
+//     }
+//     console.log(this.id);
+//     this.style.fontWeight = 600;
+//     window.setTimeout(function() {
+//         $(".scroll_window").mCustomScrollbar("destroy");
+//         $(".scroll_window").mCustomScrollbar({
+//             axis:"x", // horizontal scrollbar
+//             scrollInertia: 5,
+//             advanced:{ updateOnContentResize: true },
+//             advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
+//             advanced:{ updateOnImageLoad: true },
+//             callbacks:{
+//                 onUpdate:function(){
+//                     console.log("Scrollbars updated");
+//                 }
+//             },
+//         });
+//     }, 2000);
+//   });
 
-$(window).on("wheel", function(e){
-    var $div = $('.content');
-    // set div scroll top offset to current + extra from this scroll
-    $div.scrollTop($div.scrollTop()
-                    - e.originalEvent.wheelDelta);
+// $(window).on("wheel", function(e){
+//     var $div = $('.content');
+//     // set div scroll top offset to current + extra from this scroll
+//     $div.scrollTop($div.scrollTop()
+//                     - e.originalEvent.wheelDelta);
 
-    return false; // prevent body scrolling
-});
+//     return false; // prevent body scrolling
+// });
 
 // FUNCTIONS ===================================================================
 function setTitleIcon() {
@@ -89,20 +87,12 @@ function adjustTitleStyle() {
         title.classList.remove('fancy');
         title_h1.classList.remove('fancy');
       }
-      // title_h1.addClass('label');
-      // title_h1.classList.removeClass('fancy');
       setTitleIcon();
     } else {
       title_h1.classList.add('fancy');
       title.classList.add('fancy');
       title.classList.remove('label');
       maybeRemoveTitleIcon();
-      // title_h1.classList.removeClass('label');
-      // if (curr_icon != null) {
-      //   var icon = document.querySelector(".category .title .title_icon");
-      //   icon.classList.removeClass(curr_icon);
-      //   curr_icon = null;
-      // }
     }
   }
 }
@@ -146,6 +136,18 @@ function cleanAlbumArt() {
   document.querySelector(".pj_album_desc").innerText = "";
 }
 
+function showMiniAlbumArt() {
+  var projects = document.querySelectorAll(".row.pj_title");
+  projects.forEach(function(pj) {
+    var mini_art = document.createElement('div');
+    mini_art.classList.add('pj_album_art_mini');
+    pj.parentNode.insertBefore(mini_art, pj);
+    var pj_name = pj.innerText.toLowerCase();
+    var img_path = "assets/img/pj_album_art/" + pj_name + ".png";
+    mini_art.style.backgroundImage = "url('" + img_path + "')";
+  });
+}
+
 function maybeShowPjAlbumArt() {
   if (window.outerWidth >= 600) {
     const projects = document.querySelectorAll(".pj_name h2");
@@ -154,5 +156,15 @@ function maybeShowPjAlbumArt() {
       pj.addEventListener('mouseleave', cleanAlbumArt);
       console.log("Added mouseover listener to pj " + pj.innerText);
     });
+  } else {
+    var pj_headers = document.querySelectorAll(".pj_name h2");
+    pj_headers.forEach(function(pj) {
+      pj.removeEventListener('mouseenter', showPjAlbumArt);
+      pj.removeEventListener('mouseleave', cleanAlbumArt);
+    });
+    var mini_album_art = document.querySelectorAll(".pj_album_art_mini");
+    if (mini_album_art.length === 0) {
+      showMiniAlbumArt();
+    }
   }
 }

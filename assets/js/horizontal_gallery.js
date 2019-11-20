be9e26a27fcb7c68608e0a7f4b88d373aefe7449
Michelle Chang <mimilei@mimilei-macbookpro2.roam.corp.google.com>
2019-11-19 16:36:36 -0800

    Scrolls back to head in creative view

--- assets/js/horizontal_gallery.js
+++ assets/js/horizontal_gallery.js
@@ -1,37 +1,74 @@
-$(".subtitle.work").on("click", function(e){
-    console.log("Clicked a creative category");
-    $(".scroll").load("creative/" + this.id + ".html");
-    console.log("after loading call");
-    e.preventDefault(); // cancel the click
-    var allSubtitles = $(".subtitle.work");
-    var i;
-    for (i = 0; i < allSubtitles.length; i++) {
-        allSubtitles[i].style.fontWeight = 300;
-    }
-    console.log(this.id);
-    this.style.fontWeight = 600;
-    window.setTimeout(function() {
-        $(".scroll_window").mCustomScrollbar("destroy");
-        $(".scroll_window").mCustomScrollbar({
-            axis:"x", // horizontal scrollbar
-            scrollInertia: 5,
-            advanced:{ updateOnContentResize: true },
-            advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
-            advanced:{ updateOnImageLoad: true },
-            callbacks:{
-                onUpdate:function(){
-                    console.log("Scrollbars updated");
-                }
-            },
-        });
-    }, 2000);
-  });
-
-$(window).on("wheel", function(e){
-    var $div = $('.content');
-    // set div scroll top offset to current + extra from this scroll
-    $div.scrollTop($div.scrollTop()
-                    - e.originalEvent.wheelDelta);
-
-    return false; // prevent body scrolling
+console.log("Horizontal gallery");
+var old_category = document.querySelector("#personal_works"); // Tracks which category title to bold
+var creative_menu = document.querySelectorAll(".subtitle.work");
+creative_menu.forEach(function(item) {
+  item.addEventListener("click", onCategoryClicked);
 });
+
+// Default
+document.querySelector("#personal_works").style.fontWeight = 600;
+loadContent('creative/personal_works.html');
+
+// $(".subtitle.work").on("click", function(e){
+//     console.log("Clicked a creative category");
+//     $(".scroll").load("creative/" + this.id + ".html");
+//     console.log("after loading call");
+//     e.preventDefault(); // cancel the click
+//     var allSubtitles = $(".subtitle.work");
+//     var i;
+//     for (i = 0; i < allSubtitles.length; i++) {
+//         allSubtitles[i].style.fontWeight = 300;
+//     }
+//     console.log(this.id);
+//     this.style.fontWeight = 600;
+//     window.setTimeout(function() {
+//         $(".scroll_window").mCustomScrollbar("destroy");
+//         $(".scroll_window").mCustomScrollbar({
+//             axis:"x", // horizontal scrollbar
+//             scrollInertia: 5,
+//             advanced:{ updateOnContentResize: true },
+//             advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
+//             advanced:{ updateOnImageLoad: true },
+//             callbacks:{
+//                 onUpdate:function(){
+//                     console.log("Scrollbars updated");
+//                 }
+//             },
+//         });
+//     }, 2000);
+//   });
+
+function loadContent(str_url) {
+  console.log("Loading content from " + str_url);
+  var html = new XMLHttpRequest();
+  html.open('GET', str_url, true);
+  html.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return; // or whatever error handling you want
+      document.querySelector(".scroll_window .scroll").innerHTML= this.responseText;
+      var scroll_window = document.querySelector(".scroll_window");
+      scroll_window.scrollInertia = 5;
+      var scroll_amt = 10;
+      const scrollToStart = () => {
+        const left_pos = scroll_window.scrollLeft;
+        if (left_pos > 0) {
+          scroll_window.scrollLeft = left_pos - scroll_amt;
+          scroll_amt *= 2;
+          window.requestAnimationFrame(scrollToStart);
+        }
+      };
+      scrollToStart();
+  };
+  html.send();
+}
+
+function onCategoryClicked(event) {
+  console.log("Clicked a creative category: " + event.target.id);
+  event.target.style.fontWeight = 600;
+  if (old_category != null && old_category != event.target) {
+    old_category.style.fontWeight = 300;
+  }
+  old_category = event.target;
+  var url = "creative/" + event.target.id + ".html";
+  loadContent(url);
+}
--- assets/js/mimilei.js
+++ assets/js/mimilei.js
@@ -17,45 +17,6 @@ window.addEventListener('resize', adjustTitleStyle);
 window.addEventListener('resize', maybeShowPjAlbumArt);
 window.addEventListener('resize', maybeAddNumbering);
 
-// JQUERY ======================================================================
-// $(".subtitle.work").on("click", function(e){
-//     console.log("Clicked a creative category");
-//     $(".scroll").load("creative/" + this.id + ".html");
-//     console.log("after loading call");
-//     e.preventDefault(); // cancel the click
-//     var allSubtitles = $(".subtitle.work");
-//     var i;
-//     for (i = 0; i < allSubtitles.length; i++) {
-//         allSubtitles[i].style.fontWeight = 300;
-//     }
-//     console.log(this.id);
-//     this.style.fontWeight = 600;
-//     window.setTimeout(function() {
-//         $(".scroll_window").mCustomScrollbar("destroy");
-//         $(".scroll_window").mCustomScrollbar({
-//             axis:"x", // horizontal scrollbar
-//             scrollInertia: 5,
-//             advanced:{ updateOnContentResize: true },
-//             advanced:{ updateOnSelectorChange: "img.u-max-full-width" },
-//             advanced:{ updateOnImageLoad: true },
-//             callbacks:{
-//                 onUpdate:function(){
-//                     console.log("Scrollbars updated");
-//                 }
-//             },
-//         });
-//     }, 2000);
-//   });
-
-// $(window).on("wheel", function(e){
-//     var $div = $('.content');
-//     // set div scroll top offset to current + extra from this scroll
-//     $div.scrollTop($div.scrollTop()
-//                     - e.originalEvent.wheelDelta);
-
-//     return false; // prevent body scrolling
-// });
-
 // FUNCTIONS ===================================================================
 function setTitleIcon() {
   var icon = document.querySelector(".category .title .title_icon");
